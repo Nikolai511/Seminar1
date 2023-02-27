4c3441d59537449d9d0c9efd83b0fa7afc3b2951
Nikolai Narvatkin <nikki500111@gmail.com>
2023-02-27 15:27:08 +0300

    Домашняя работа

---
+++ Задача2/Задача3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая на вход принимает число и выдает,
+// является ли число четным (делится ли оно на два без остатка)
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Div = number % 2;
+
+if(Div == 0)
+{
+Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет");
+}
+
+
+
+
+
+
+
